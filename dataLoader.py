423966b2f2f5d0cdfc0e89aa167b7a790bf1491f
Zhao YongChi <lwyeah@qq.com>
2020-02-16 23:08:55 +0800

    no sense

--- dataLoader.py
+++ dataLoader.py
@@ -10,6 +10,8 @@ from random import shuffle
 
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
+token_table = {'ecommerce': 'electronic commerce'}
+
 
 class ProgramWebDataset(Dataset):
     def __init__(self, data, co_occur_mat, tag2id, id2tag=None):
@@ -51,10 +53,12 @@ class ProgramWebDataset(Dataset):
                 dscp_ids = tokenizer.convert_tokens_to_ids(dscp_tokens)
                 tag = tag.strip().split('###')
                 for t in tag:
+                    if t == '':
+                        print(tag, '|||', row)
                     if t not in tag2id:
-                        tag_tokens = tokenizer.tokenize(t)
-                        if np.any([token.startswith('##') for token in tag_tokens]):
-                            print(t, ':', tag_tokens)
+                        # tag_tokens = tokenizer.tokenize(t)
+                        # if np.any([token.startswith('##') for token in tag_tokens]):
+                        #     print(t, ':', tag_tokens)
                         tag_id = len(tag2id)
                         tag2id[t] = tag_id
                         id2tag[tag_id] = t
