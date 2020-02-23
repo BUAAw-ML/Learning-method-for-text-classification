c2bce2a5ef2e94a25d98e6909d0c19ffaf09bb61
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-02-23 12:25:00 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -93,7 +93,7 @@ class ProgramWebDataset(Dataset):
 
                 tag1 = tag1.strip()
                 tag2 = tag2.strip()
-
+                print(float(similar))
                 co_occur_mat[tag2id[tag1], tag2id[tag2]] += float(similar)
 
         return co_occur_mat
