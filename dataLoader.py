518a5ac2315e1c46f04dffbd5830040ed44ce868
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-02-27 10:46:23 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -50,8 +50,8 @@ class ProgramWebDataset(Dataset):
                 if len(row) != 4:
                     continue
                 id, title, dscp, tag = row
-                if len(dscp) > 1100:
-                    continue
+                # if len(dscp) > 1100:
+                #     continue
 
                 title_tokens = tokenizer.tokenize(title.strip())
                 dscp_tokens = tokenizer.tokenize(dscp.strip())
