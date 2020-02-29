f7aa814e757a3ca89d39980a1058ecd5e8aad951
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-02-29 11:34:30 +0800

    update

--- models.py
+++ models.py
@@ -66,18 +66,17 @@ class GCNBert(nn.Module):
             attention_mask=attention_mask)[0]
         sentence_feat = torch.sum(token_feat * attention_mask.unsqueeze(-1), dim=1) \
             / torch.sum(attention_mask, dim=1, keepdim=True)
-        sentence_feat = sentence_feat.cpu()
-        print(type(sentence_feat))
+
         embed = self.bert.get_input_embeddings()
         tag_embedding = embed(encoded_tag)
-        # tag_embedding = torch.sum(tag_embedding * tag_mask.unsqueeze(-1), dim=1) \
-        #     / torch.sum(tag_mask, dim=1, keepdim=True)
-        # x = self.gc1(tag_embedding, self.adj)
-        # x = self.relu(x)
-        # x = self.gc2(x, self.adj)
-        #
-        # x = x.transpose(0, 1)
-        # x = torch.matmul(sentence_feat, x)
+        tag_embedding = torch.sum(tag_embedding * tag_mask.unsqueeze(-1), dim=1) \
+            / torch.sum(tag_mask, dim=1, keepdim=True)
+        x = self.gc1(tag_embedding, self.adj)
+        x = self.relu(x)
+        x = self.gc2(x, self.adj)
+
+        x = x.transpose(0, 1)
+        x = torch.matmul(sentence_feat, x)
 
         linear = nn.Linear(len(sentence_feat), len(tag_embedding))
         x = linear(sentence_feat)
