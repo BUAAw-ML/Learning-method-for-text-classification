3d2948c63ac5e705592ce187cabd1566edd0b6a7
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-04-10 10:40:01 +0800

    update

--- dataLoader.py
+++ dataLoader.py
@@ -1,50 +1,86 @@
 import csv
 import copy
 import os
-
+import sys
 from random import shuffle
-
 import numpy as np
 import torch
 from torch.utils.data import Dataset
 from transformers import BertTokenizer
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from word_embedding import *
 
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-
 token_table = {'ecommerce': 'electronic commerce'}
 
 
-class ProgramWebDataset(Dataset):
-    def __init__(self, data, co_occur_mat, tag2id, id2tag=None):
+class allDataObject(Dataset):
+    def __init__(self, data, co_occur_mat, tag2id, id2tag=None, tfidf_dict=None):
         self.data = data
         self.co_occur_mat = co_occur_mat
         self.tag2id = tag2id
         if id2tag is None:
             id2tag = {v: k for k, v in tag2id.items()}
         self.id2tag = id2tag
+        self.tfidf_dict = tfidf_dict
 
     @classmethod
     def from_dict(cls, data_dict):
-        return ProgramWebDataset(data_dict.get('data'),
+        return allDataObject(data_dict.get('data'),
                                  data_dict.get('co_occur_mat'),
                                  data_dict.get('tag2id'),
                                  data_dict.get('id2tag'))
 
     @classmethod
-    def from_csv(cls, api_csvfile, net_csvfile, ignored_tags=None):
-        data, tag2id, id2tag = ProgramWebDataset.load(api_csvfile, ignored_tags=ignored_tags)
-        co_occur_mat = ProgramWebDataset.stat_cooccurence(data, len(tag2id))
-        #co_occur_mat = ProgramWebDataset.similar_net(net_csvfile, tag2id)
-        return ProgramWebDataset(data, co_occur_mat, tag2id, id2tag)
+    def from_csv(cls, all_data_file, net_csvfile):
+        data, tag2id, id2tag, document,tag_based = allDataObject.load_programWeb(all_data_file)
+        co_occur_mat = allDataObject.stat_cooccurence(data,len(tag2id))
+
+        tfidf_dict = allDataObject.get_tfidf_dict(document)
+
+        return allDataObject(data, co_occur_mat, tag2id, id2tag, tfidf_dict)
 
     @classmethod
-    def load(cls, f, ignored_tags=None):
+    def load_programWeb(cls, f):
         data = []
         tag2id = {}
         id2tag = {}
+
+        tag2token = {}
+        tag_based = {}
+
+        document = []
+        tag_occurance = {}
+        #csv.field_size_limit(500 * 1024 * 1024)
+        #csv.field_size_limit(sys.maxsize)
         with open(f, newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',')
+            reader = csv.reader(csvfile)#, delimiter=',')
+            next(reader)
+            for row in reader:
+
+                if len(row) != 4:
+                    continue
+                _, _, _, tag = row
+
+                tag = tag.strip().split('###') # '###'
+                tag = [t for t in tag if t != '']
+
+                for t in tag:
+                    if t not in tag_occurance:
+                        tag_occurance[t] = 1
+                    tag_occurance[t] += 1
+
+        ignored_tags = set(['Tools','Applications','Other', 'API', 'Software-as-a-Service','Platform-as-a-Service','Data-as-a-Service'])  #
+        for tag in tag_occurance:
+            if tag_occurance[tag] < 0:
+                ignored_tags.add(tag)
+
+        print(ignored_tags)
+
+        with open(f, newline='') as csvfile:
+            reader = csv.reader(csvfile)#, delimiter=',')
             next(reader)
             for row in reader:
                 if len(row) != 4:
@@ -53,45 +89,74 @@ class ProgramWebDataset(Dataset):
 
                 title_tokens = tokenizer.tokenize(title.strip())
                 dscp_tokens = tokenizer.tokenize(dscp.strip())
-                if len(dscp_tokens) > 500:
+                if len(title_tokens) + len(dscp_tokens) > 510:
                     continue
+
+                document.append(" ".join(title_tokens) + " ".join(dscp_tokens))
+
                 title_ids = tokenizer.convert_tokens_to_ids(title_tokens)
                 dscp_ids = tokenizer.convert_tokens_to_ids(dscp_tokens)
+
                 tag = tag.strip().split('###')
                 tag = [t for t in tag if t != '']
+
                 if ignored_tags is not None:
                     tag = [t for t in tag if t not in ignored_tags]
+
+                # if len(set(tag)) < 2:
+                #     continue
+
                 if len(tag) == 0:
                     continue
+
                 for t in tag:
                     if t not in tag2id:
-                        # tag_tokens = tokenizer.tokenize(t)
-                        # if np.any([token.startswith('##') for token in tag_tokens]):
-                        #     print(t, ':', tag_tokens)
                         tag_id = len(tag2id)
                         tag2id[t] = tag_id
                         id2tag[tag_id] = t
+
                 tag_ids = [tag2id[t] for t in tag]
+
                 data.append({
                     'id': int(id),
-                    'title_ids': title_ids,
-                    'title_tokens': title_tokens,
-                    'dscp_ids': dscp_ids,
-                    'dscp_tokens': dscp_tokens,
+                    'dscp_ids': title_ids + dscp_ids,
+                    'dscp_tokens': title_tokens + dscp_tokens,
                     'tag_ids': tag_ids,
                     'dscp': dscp
                 })
+
+        print("The number of tags for training: {}".format(len(tag2id)))
         os.makedirs('cache', exist_ok=True)
-        return data, tag2id, id2tag
+
+        return data, tag2id, id2tag, document, tag_based
+
+    @classmethod
+    def get_tfidf_dict(cls, document):
+        tfidf_dict = {}
+        tfidf_model = TfidfVectorizer(sublinear_tf=True,
+                                        strip_accents='unicode',
+                                        analyzer='word',
+                                        token_pattern=r'\w{1,}',
+                                        stop_words='english',
+                                        ngram_range=(1, 1),
+                                        max_features=10000).fit(document)
+        for item in tfidf_model.vocabulary_:
+            tfidf_dict[item] = tfidf_model.idf_[tfidf_model.vocabulary_[item]]
+
+        return tfidf_dict
+
 
     @classmethod
     def stat_cooccurence(cls, data, tags_num):
+
         co_occur_mat = torch.zeros(size=(tags_num, tags_num))
         for i in range(len(data)):
             tag_ids = data[i]['tag_ids']
             for t1 in range(len(tag_ids)):
                 for t2 in range(len(tag_ids)):
+                    #if tag_ids[t1] != tag_ids[t2]:
                     co_occur_mat[tag_ids[t1], tag_ids[t2]] += 1
+
         return co_occur_mat
 
     @classmethod
@@ -155,14 +220,38 @@ class ProgramWebDataset(Dataset):
             padded_tag_ids[i, :len(tag_ids[i])] = torch.tensor(tag_ids[i])
         return padded_tag_ids, mask
 
+    def obtain_tag_embedding(self, wv='glove', model_path='data'):
+
+        if wv == 'glove':
+            save_file = os.path.join('data', 'word_embedding_model', 'glove_word2vec_wordnet.pkl')
+            if not os.path.exists(save_file):
+                word_vectors = get_glove_dict(model_path)
+        else:
+            raise NotImplementedError
+
+        if not os.path.exists(save_file):
+            tag_list = []
+            for i in range(self.get_tags_num()):
+                tag = self.id2tag[i]
+                tag_list.append(tag)
+            print(tag_list)
+
+            print('obtain semantic word embedding', save_file)
+            embed_text_file(tag_list, word_vectors, save_file)
+        else:
+            print('Embedding existed :', save_file, 'Skip!!!')
+
+        return save_file
+
     def collate_fn(self, batch):
         result = {}
         # construct input
-        inputs = [e['title_ids'] + e['dscp_ids'] for e in batch]
-        # inputs = [e['dscp_ids'] for e in batch]
+        inputs = [e['dscp_ids'] for e in batch]  #e['title_ids'] +
+
         lengths = np.array([len(e) for e in inputs])
         max_len = np.max(lengths)
-        inputs = [tokenizer.prepare_for_model(e, max_length=max_len + 2, pad_to_max_length=True) for e in inputs]
+        inputs = [tokenizer.prepare_for_model(e, max_length=max_len+2, pad_to_max_length=True) for e in inputs]
+
         ids = torch.LongTensor([e['input_ids'] for e in inputs])
         token_type_ids = torch.LongTensor([e['token_type_ids'] for e in inputs])
         attention_mask = torch.FloatTensor([e['attention_mask'] for e in inputs])
@@ -172,12 +261,13 @@ class ProgramWebDataset(Dataset):
             tags[i, batch[i]['tag_ids']] = 1.
 
         dscp = [e['dscp'] for e in batch]
+
         return (ids, token_type_ids, attention_mask), tags, dscp
 
 
-def load_dataset(api_csvfile=None, net_csvfile=None):
+def load_dataset(all_data_file=None, net_csvfile=None):
 
-    cache_file_head = api_csvfile.split("/")[-1]
+    cache_file_head = all_data_file.split("/")[-1]
 
     if os.path.isfile(os.path.join('cache', cache_file_head + '.train')) \
             and os.path.isfile(os.path.join('cache', cache_file_head + '.eval')) \
@@ -186,8 +276,8 @@ def load_dataset(api_csvfile=None, net_csvfile=None):
 
         print("load dataset from cache")
 
-        train_dataset, val_dataset = ProgramWebDataset.from_dict(
-            torch.load(os.path.join('cache', cache_file_head + '.train'))), ProgramWebDataset.from_dict(
+        train_dataset, val_dataset = allDataObject.from_dict(
+            torch.load(os.path.join('cache', cache_file_head + '.train'))), allDataObject.from_dict(
             torch.load(os.path.join('cache', cache_file_head + '.eval')))
         encoded_tag, tag_mask = torch.load(os.path.join('cache', cache_file_head + '.encoded_tag')), \
                                 torch.load(os.path.join('cache', cache_file_head + '.tag_mask'))
@@ -199,8 +289,7 @@ def load_dataset(api_csvfile=None, net_csvfile=None):
         if not os.path.exists('cache'):
             os.makedirs('cache')
 
-        ignored_tags = torch.load('./cache/ignored_tags')
-        dataset = ProgramWebDataset.from_csv(api_csvfile, net_csvfile, ignored_tags=ignored_tags)
+        dataset = allDataObject.from_csv(all_data_file, net_csvfile)
 
         encoded_tag, tag_mask = dataset.encode_tag()
 
@@ -210,15 +299,17 @@ def load_dataset(api_csvfile=None, net_csvfile=None):
         data = np.array(dataset.data)
         train_dataset = dataset
         val_dataset = copy.copy(dataset)
-        ind = np.random.permutation(len(data))
-
-        train_dataset.data = data[ind[:-2000]].tolist()
-        val_dataset.data = data[ind[-2000:]].tolist()
+        ind = np.random.RandomState(seed=10).permutation(len(data))
+        split = int(len(data) * 0.85)
+        train_dataset.data = data[ind[:split]].tolist()
+        val_dataset.data = data[ind[split:]].tolist()
 
         torch.save(train_dataset.to_dict(), os.path.join('cache', cache_file_head + '.train'))
         torch.save(val_dataset.to_dict(), os.path.join('cache', cache_file_head + '.eval'))
 
     print("train_data_size: {}".format(len(train_dataset.data)))
     print("val_data_size: {}".format(len(val_dataset.data)))
+
+    tag_embedding_file = dataset.obtain_tag_embedding()
 
     return train_dataset, val_dataset, encoded_tag, tag_mask
