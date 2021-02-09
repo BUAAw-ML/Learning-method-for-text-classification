5d0d5b8e3862eebf5a19059201bfe13fe04a2497
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-02-09 12:54:00 +0800

    update

--- engine.py
+++ engine.py
@@ -514,8 +514,8 @@ class semiGAN_MultiLabelMAPEngine(MultiLabelMAPEngine):
 
         self.state['output'] = logits
 
-        D_L_unsupervised = -1 * torch.mean(torch.log(1 - prob + epsilon))
-        D_L_unsupervised2 = -1 * torch.mean(torch.log(flatten + epsilon))
+        D_L_unsupervised = -1 * torch.mean(torch.log(prob + epsilon))
+        D_L_unsupervised2 = -1 * torch.mean(torch.log(1 -  flatten + epsilon))
 
         if semi_supervised == False:  # train with labeled data
             d_loss = criterion(self.state['output'], target_var)
