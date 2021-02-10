b1bceb1b10faead314b15456cd72d5df44bf09a5
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-02-10 09:19:19 +0800

    update

--- engine.py
+++ engine.py
@@ -510,7 +510,7 @@ class semiGAN_MultiLabelMAPEngine(MultiLabelMAPEngine):
         # -----------train enc-----------
         flatten, logits, prob, attention = model['Classifier'](ids, token_type_ids, attention_mask,
                                                 self.state['encoded_tag'],
-                                                self.state['tag_mask'], x_g.detach())  #
+                                                self.state['tag_mask'], z)  # x_g.detach()
 
         self.state['output'] = logits
 
