ec647a94ff04bdb66486f60eb748809267f12618
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-02-24 09:20:00 +0800

    update

--- models.py
+++ models.py
@@ -38,7 +38,7 @@ class MABert(nn.Module):
 
         fake_ids = ids.clone()#.detach() torch.Tensor(fake_ids.shape[0], fake_ids.shape[1]).uniform_(150, 1000).long().cuda(0)
 
-        fake_ids = torch.where(fake_ids > 102, fake_ids-1, fake_ids)
+        fake_ids = torch.where(fake_ids > 102, torch.Tensor(fake_ids.shape[0], fake_ids.shape[1]).uniform_(150, 30000).long().cuda(0), fake_ids)
         # fake_ids[fake_ids > 102] -=
 
         feat = self.bert(fake_ids,
@@ -63,7 +63,7 @@ class MABert(nn.Module):
 
         attention = F.softmax(attention, -1)
 
-        attention_out = attention @ token_feat   # N, labels_num, hidden_size
+        attention_out = torch.matmul(attention, token_feat)   # N, labels_num, hidden_size
 
         attention_out = attention_out * self.class_weight
         attention_out = torch.sum(attention_out, -1)
@@ -90,7 +90,7 @@ class MABert(nn.Module):
 
         # print('+',torch.max(attention_fake,-1)[0])
 
-        attention_out_fake = attention_fake @ feat  # N, labels_num, hidden_size
+        attention_out_fake = torch.matmul(attention_fake, feat)  # N, labels_num, hidden_size
         # discrimate = torch.matmul(feat, tag_embedding.transpose(0, 1))
         attention_out_fake = attention_out_fake * self.class_weight
         attention_out_fake = torch.sum(attention_out_fake, -1)
