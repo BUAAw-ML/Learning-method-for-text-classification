01b1518427abecff56ace61e8e7c97ea0071f06b
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-02-24 16:14:11 +0800

    update

--- models.py
+++ models.py
@@ -104,11 +104,11 @@ class MABert(nn.Module):
         attention_out_fake = hidden_out_fake[:, :-1]
 
         attention_out_fake = attention_out_fake * self.class_weight
-        attention_out_fake = torch.sum(attention_out_fake, -1)
+        attention_out_fake = torch.sum(attention_out_fake, -1, keepdim=True)
 
         discrimate_hidden = torch.sum(torch.matmul(hidden_out_fake[:,-1].squeeze(-2), self.class_weight.transpose(0, 1)),-1)
-        print(attention_out_fake.shape)
-        print(discrimate_hidden.shape)
+        # print(attention_out_fake.shape)
+        # print(discrimate_hidden.shape)
 
         attention_out_fake = torch.cat((attention_out_fake, discrimate_hidden), -1)
 
