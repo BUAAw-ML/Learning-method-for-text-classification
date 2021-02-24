7942bf626a3a38de9714ab2f176d5df89ddff74f
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-02-24 09:29:09 +0800

    update

--- models.py
+++ models.py
@@ -123,6 +123,8 @@ class MABert(nn.Module):
         attention_out_fake = torch.sum(attention_out_fake, -1, keepdim=True)
 
         prob = torch.cat((attention_out_fake,attention_out),-1)
+        print(prob)
+
         prob = torch.sigmoid(prob)
 
 
