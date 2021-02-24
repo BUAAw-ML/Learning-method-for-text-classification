b31f45e0775f8612440c1496e276fc0b2751ae5b
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-02-24 16:11:38 +0800

    update

--- models.py
+++ models.py
@@ -106,8 +106,8 @@ class MABert(nn.Module):
         attention_out_fake = attention_out_fake * self.class_weight
         attention_out_fake = torch.sum(attention_out_fake, -1)
 
-        print(torch.matmul(hidden_out_fake[:,-1].squeeze(-2), self.class_weight.transpose(0, 1)).shape)
-        attention_out_fake = torch.cat((attention_out_fake, torch.matmul(hidden_out_fake[:,-1].squeeze(-2), self.class_weight.transpose(0, 1))), -1)
+        discrimate_hidden = torch.sum(torch.matmul(hidden_out_fake[:,-1].squeeze(-2), self.class_weight.transpose(0, 1)),-1)
+        attention_out_fake = torch.cat((attention_out_fake, discrimate_hidden), -1)
 
         prob = self.output(attention_out_fake)[:, -1]
 
