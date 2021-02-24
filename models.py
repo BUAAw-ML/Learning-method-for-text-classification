06883d0b88f3749cbc1a93a547658acc2967403c
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-02-24 10:24:13 +0800

    update

--- models.py
+++ models.py
@@ -67,11 +67,11 @@ class MABert(nn.Module):
 
         #################fake sample process#######
 
-        print(torch.sum(attention,-2))
+        # print(torch.sum(attention,-2))
 
         fake_ids = ids.clone()#.detach() torch.Tensor(fake_ids.shape[0], fake_ids.shape[1]).uniform_(150, 1000).long().cuda(0)
 
-        fake_ids = torch.where(fake_ids > 102, torch.Tensor(fake_ids.shape[0], fake_ids.shape[1]).uniform_(150, 30000).long().cuda(0), fake_ids)
+        fake_ids = torch.where(torch.sum(attention,-2) > 1, torch.Tensor(fake_ids.shape[0], fake_ids.shape[1]).uniform_(150, 30000).long().cuda(0), fake_ids)
         # fake_ids[fake_ids > 102] -=
 
         feat = self.bert(fake_ids,
@@ -122,8 +122,8 @@ class MABert(nn.Module):
 
         # print(torch.sum(attention_out_fake - attention_out > 0, -1))
 
-        # attention_out = torch.sum(attention_out, -1, keepdim=True)
-        # attention_out_fake = torch.sum(attention_out_fake, -1, keepdim=True)
+        attention_out = torch.sum(attention_out, -1, keepdim=True)
+        attention_out_fake = torch.sum(attention_out_fake, -1, keepdim=True)
 
         prob = torch.cat((attention_out_fake,attention_out),-1)
 
