830d867da39a772f12d86baa20f45ab1ae20db72
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-02-24 08:42:03 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -17,7 +17,7 @@ python main.py \
         --save_model_path='./checkpoint' \
         --data_type='TrainTest_programWeb_freecode_AAPD' \
         --data_path='../datasets/AAPD' \
-        --use_previousData=1 \
+        --use_previousData=0 \
         --model_type='MABert' \
         --method='semiGAN_MultiLabelMAP' \
         --overlength_handle='truncation' \
