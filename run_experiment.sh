a7e2a9aae5e893a1dad68fe5b5e79d6f72c6ebf5
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-02-26 15:27:52 +0800

    update

--- run_experiment.sh
+++ run_experiment.sh
@@ -19,7 +19,7 @@ python main.py \
         --model_type='MABert' \
         --method='MultiLabelMAP' \
         --overlength_handle='truncation' \
-        --min_tagFrequence=10  \
+        --min_tagFrequence=250  \
         --max_tagFrequence=999999  \
         --intanceNum_limit=999999 \
         --data_split=999999  \
